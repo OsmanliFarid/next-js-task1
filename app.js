9902080ba1fd749b7c5cc5d1ab239cc78ec68067
OsmanliFarid <osmanliferid686@gmail.com>
2025-05-06 14:03:19 +0400

    js change

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-const hesablaba = (num1,num2,) =>{
+import fs from "fs"
+let num = 1
+let mesaj2 = ""
+function vurma(){
+   for(let i = 1;i<=9;i++){
+    for(let j = 1;j<=9;j++){
+        
+        
+        
+        let mesaj = `${num} * ${j} = ${num * j}`
+        mesaj2 = String(mesaj)
+        console.log(mesaj);
+        
+    }
+    num++
     
-    let mesaj  = `${num1} + ${num2} = ${num1 + num2}`
-    console.log(mesaj);
     
+    
+   }
 }
- hesablaba(5,6)
+vurma(num)
+fs.writeFile("./db.txt", mesaj2, () =>{
+    mesaj2++
+    console.log("salam");
+    
+})
