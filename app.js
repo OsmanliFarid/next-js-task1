6c5e359095cf2e3a0590089a6ad021625315638b
OsmanliFarid <osmanliferid686@gmail.com>
2025-05-07 23:31:04 +0400

    app.js change

--- app.js
+++ app.js
@@ -1,26 +1,4 @@
 import fs from "fs"
-let num = 1
-let mesaj2 = ""
-function vurma(){
-   for(let i = 1;i<=9;i++){
-    for(let j = 1;j<=9;j++){
-        
-        
-        
-        let mesaj = `${num} * ${j} = ${num * j}`
-        mesaj2 = String(mesaj)
-        console.log(mesaj);
-        
-    }
-    num++
-    
-    
-    
-   }
-}
-vurma(num)
-fs.writeFile("./db.txt", mesaj2, () =>{
-    mesaj2++
-    console.log("salam");
-    
-})
+import { func } from "./sec.js" 
+func()
+console.log(func);
